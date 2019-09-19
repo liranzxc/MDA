bffbd0c7d4aa63a727c3e1f28f9c298c4ee397a3
GalMadar1 <galmadar1@gmail.com>
2019-09-20 00:03:10 +0300

    gal

--- EvacuationArea/EvacuationMain.py
+++ EvacuationArea/EvacuationMain.py
@@ -1,8 +1,10 @@
 import threading, sys
-import random, pika
+import random, pika, operator
 
 from inAmbulance import Threaded_Ambulance
 from inPatient import Threaded_Patient
+from ThreadAmbulanceBack import ThreadAmbulanceBack
+from TreatmentConsumer import UpdateProbability
 
 ambulances = []
 patients = []
@@ -17,33 +19,149 @@ patient_thread.start()
 while True:
     time.sleep(1)
     currentTime += 1
-    randomEvac()
+    random_evacuation()
+    fifo()
 
 
-def randomEvac():
-
+def random_evacuation():
     ambulance = random.choice(ambulances)
-    patient = random.choice(patients)
+    if ambulance['type'] is "ALS":
+        patient = random.choice(list(filter(lambda d: d['type'] is 'u' or 'd', patients)))
+    else:
+        patient = random.choice(list(filter(lambda d: d['type'] is 'n', patients)))
     print("patient %r in ambulance %r" %(patient, ambulance))
     ambulances.remove(ambulance)
     patients.remove(patient)
-    returnAmbulance()
+    return_ambulance = ThreadAmbulanceBack(ambulance, currentTime)
+    return_ambulance.start()
+    UpdateProbability(list(filter(lambda d: d['type'] is 'u', patients)),
+                      list(filter(lambda d: d['type'] is 'n', patients)),
+                      list(filter(lambda d: d['type'] is 'd', patients)),
+                      0)
 
-def returnAmbulance():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='192.168.43.136'))
-    channel = connection.channel()
 
-    channel.queue_declare(queue='task_queue', durable=True)
+def fifo():
+    i = 0
+    flag = False
+    ambulance = ambulances[0]
+    if ambulance['type'] is "ALS":
+        while flag is False:
+            patient = patients[i]
+            if patient['type'] is "u":
+                flag = True
+            i += 1
+    else:
+        while flag is False:
+            patient = patients[i]
+            if patient['type'] is "n":
+                flag = True
+            i += 1
+    print("patient %r in ambulance %r" % (patient, ambulance))
+    ambulances.remove(ambulance)
+    patients.remove(patient)
+    return_ambulance = ThreadAmbulanceBack(ambulance, currentTime)
+    return_ambulance.start()
+    UpdateProbability(list(filter(lambda d: d['type'] is 'u', patients)),
+                      list(filter(lambda d: d['type'] is 'n', patients)),
+                      list(filter(lambda d: d['type'] is 'd', patients)),
+                      0.5)
 
-    message = ' '.join(sys.argv[1:]) or "Hello liran World!"
+def urgent_fifo():
+    i = 0
+    j = 0
+    flag_ambulance = False
+    flag_patient = False
+    while flag_ambulance is False:
+        ambulance = ambulances[i]
+        if ambulance['type'] is "ALS":
+            flag_ambulance = True
+        i += 1
+    while flag_patient is False:
+        patient = patients[j]
+        if patient['type'] is "u":
+            flag_patient = True
+        j += 1
+    print("patient %r in ambulance %r" % (patient, ambulance))
+    ambulances.remove(ambulance)
+    patients.remove(patient)
+    return_ambulance = ThreadAmbulanceBack(ambulance, currentTime)
+    return_ambulance.start()
+    UpdateProbability(list(filter(lambda d: d['type'] is 'u', patients)),
+                      list(filter(lambda d: d['type'] is 'n', patients)),
+                      list(filter(lambda d: d['type'] is 'd', patients)),
+                      1)
 
-    channel.basic_publish(
-        exchange='',
-        routing_key='task_queue',
-        body=message,
-        properties=pika.BasicProperties(
-            delivery_mode=2,  # make message persistent
-        ))
-    print(" [x] Sent %r" % message)
-    connection.close()
+
+def full_capacity():
+    ambulances.sort(key=operator.itemgetter('type'))
+    count = -1
+    for i in patients:
+        count += 1
+        if patients[count]['in_ambulance'] is False:
+            for j in range(0, len(ambulances)):
+                if ambulances[j]['type'] is "ALS":
+                    if patients[count]['type'] is "u":
+                        if len(ambulances[j]['u']) < 1:
+                            ambulances[j]['u'].append(patient)
+                            print("patient %r in ambulance %r" % (patients[count], ambulances[j]))
+                            patients[count]['in_ambulance'] = True
+                            break
+                        continue
+                    if patients[count]['type'] is "n":
+                        if len(ambulances[j]['n']) < 2:
+                            ambulances[j]['n'].append(patient)
+                            print("patient %r in ambulance %r" % (patients[count], ambulances[j]))
+                            patients[count]['in_ambulance'] = True
+                            break
+                        continue
+                if ambulances[j]['type'] is "BLS":
+                    if patients[count]['type'] is "n":
+                        if len(ambulances[j]['n']) < 3:
+                            ambulances[j]['n'].append(patient)
+                            print("patient %r in ambulance %r" % (patients[count], ambulances[j]))
+                            patients[count]['in_ambulance'] = True
+                            break
+                        continue
+    send_ambulances = []
+    for ambulance in ambulances:
+        if ambulance['type'] is 'ALS':
+            if len(ambulance['u']) == 1 and len(ambulance['n']) == 2:
+                send_ambulances.append(ambulance)
+                for i in range(0, len(ambulance['n'])):
+                    patients.remove(ambulance['n'][i])
+                patients.remove(ambulance['u'][0])
+        else:
+            if len(ambulance['n']) == 3:
+                send_ambulances.append(ambulance)
+            for i in range(0, len(ambulance['n'])):
+                patients.remove(ambulance['n'][i])
+        ambulances.remove(ambulance)
+        return_ambulance = ThreadAmbulanceBack(ambulance, currentTime)
+        return_ambulance.start()
+        UpdateProbability(list(filter(lambda d: d['type'] is 'u', patients)),
+                          list(filter(lambda d: d['type'] is 'n', patients)),
+                          list(filter(lambda d: d['type'] is 'd', patients)),
+                          1)
+
+
+def triage():
+    for patient in patients:
+        if patient['type'] is "u":
+            als_ambulances = list(filter(lambda d: d['type'] is 'ALS', ambulances))
+            if len(als_ambulances) != 0:
+                print("patient %r in ambulance %r" % (patient, als_ambulances[0]))
+                ambulances.remove(als_ambulances[0])
+                patients.remove(patient)
+                return_ambulance = ThreadAmbulanceBack(als_ambulances[0], currentTime)
+                return_ambulance.start()
+        else:
+            print("patient %r in ambulance %r" % (patient, ambulances[0]))
+            ambulance = ambulances[0]
+            ambulances.remove(ambulance)
+            patients.remove(patient)
+            return_ambulance = ThreadAmbulanceBack(ambulance, currentTime)
+            return_ambulance.start()
+            UpdateProbability(list(filter(lambda d: d['type'] is 'u', patients)),
+                              list(filter(lambda d: d['type'] is 'n', patients)),
+                              list(filter(lambda d: d['type'] is 'd', patients)),
+                              2)
