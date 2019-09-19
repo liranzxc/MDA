f07ca1c7aefeb561913b617094297cfb77a3bdeb
GalMadar1 <galmadar1@gmail.com>
2019-09-19 21:08:29 +0300

    gal

--- EvacuationArea/EvacuationMain.py
+++ EvacuationArea/EvacuationMain.py
@@ -1,12 +1,49 @@
-import threading
+import threading, sys
+import random, pika
 
 from inAmbulance import Threaded_Ambulance
 from inPatient import Threaded_Patient
 
-ambulance_thread = Threaded_Ambulance([])
+ambulances = []
+patients = []
+currentTime = 0
 
-patient_thread = Threaded_Patient([])
+ambulance_thread = Threaded_Ambulance(ambulances)
+patient_thread = Threaded_Patient(patients)
 
 ambulance_thread.start()
-
 patient_thread.start()
+
+while True:
+    time.sleep(1)
+    currentTime += 1
+    randomEvac()
+
+
+def randomEvac():
+
+    ambulance = random.choice(ambulances)
+    patient = random.choice(patients)
+    print("patient %r in ambulance %r" %(patient, ambulance))
+    ambulances.remove(ambulance)
+    patients.remove(patient)
+    returnAmbulance()
+
+def returnAmbulance():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='192.168.43.136'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='task_queue', durable=True)
+
+    message = ' '.join(sys.argv[1:]) or "Hello liran World!"
+
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    print(" [x] Sent %r" % message)
+    connection.close()
