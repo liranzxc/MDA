a8d55a3195462dd1a2a6c94bdb1503478c990f44
danielmar121 <danielmar121@gmail.com>
2019-09-19 18:11:35 +0300

    image

--- publisherP/publisherP.py
+++ publisherP/publisherP.py
@@ -1,53 +1,13 @@
 #!/usr/bin/env python
-import pika
 import sys
-import json
-import random
-
-CURRENT_TIME = 0
-
-def publish_patient(patient):
-    channel.basic_publish(
-        exchange='',
-        routing_key='Patients_queue',
-        body=patient,
-        properties=pika.BasicProperties(
-            delivery_mode=2,  # make message persistent
-        ))
-
-def make_patient(max, t_zero, ran_from, ran_to, patient_type):
-    counter = 1
-    sleep(t_zero)
-    cur_time = t_zero
-    while counter <= max:
-        time_to_send = (random.randint(ran_from, ran_to) / 10)
-        sleep(time_to_send)
-        cur_time = cur_time + time_to_send
-        patient = {
-          "id" : counter ,
-          "type"  : patient_type,
-          "tZero" : t_zero,
-          'probability' : 0.9 ,
-          "cur_time" : cur_time ,
-        }
-        print(json.dumps(patient))
-        # publish_patient(json.dumps(patient))
-        counter += 1
+from threadsPatient import PatientThread
 
 if __name__ == "__main__":
-    #start rabbitMQ
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='192.168.43.136'))
-    channel = connection.channel()
-    channel.queue_declare(queue='Patients_queue', durable=True)
-    message = ' '.join(sys.argv[1:]) or "Hello liran World!"
-
     #star threads
+    non_urgent = PatientThread(75, 0, 5, 15, 'n',0.9)
+    urgent = PatientThread(45, 15, 15, 25, 'u',0.5)
+    dead = PatientThread(30, 20, 10, 20, 'd',0)
 
-    #change current time by 0.1 min
-    while True:
-        CURRENT_TIME +=0.1
-        sleep(0.1)
-
-    connection.close()
-
+    non_urgent.start()
+    urgent.start()
+    dead.start()
---
+++ publisherP/threadsPatient.py
@@ -0,0 +1,63 @@
+from threading import Thread
+import uuid
+import pika
+import json
+import random
+from time import sleep
+
+
+class PatientThread(Thread):
+
+    def __init__(self, max_patient, t_zero, ran_from, ran_to, patient_type,prob):
+        ''' Constructor. '''
+        Thread.__init__(self)
+        self.max_patient = max_patient
+        self.t_zero = t_zero
+        self.ran_from = ran_from
+        self.ran_to = ran_to
+        self.patient_type = patient_type
+        self.current_patients = 0
+        self.patient = {
+          "id" : 0 ,
+          "type"  : '',
+          "tZero" : 0,
+          'probability' : 0 ,
+          "cur_time" : 0 ,
+        }
+        self.probability = prob
+        self.current_time = t_zero
+        self.channel = ''
+
+    def publish_patient(self, patient, ):
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='Patients_queue',
+            body=patient,
+            properties=pika.BasicProperties(
+                delivery_mode=2,  # make message persistent
+            ))
+
+    def run(self):
+        # start rabbitMQ Connection
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='192.168.43.136'))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue='Patients_queue', durable=True)
+
+        #sleep until the time to start send
+        sleep(self.t_zero)
+        #sending patient to queue
+        while self.current_patients < self.max_patient:
+            # Changing patient Details
+            self.patient['id'] = str(uuid.uuid1())
+            self.patient['type'] = self.patient_type
+            self.patient['tZero'] = self.t_zero
+            self.patient['probability'] = self.probability
+            time_to_send = (random.randint(self.ran_from, self.ran_to) / 10)
+            sleep(time_to_send)
+            self.current_time = self.current_time + time_to_send
+            self.patient['cur_time'] = self.current_time
+            self.publish_patient(json.dumps(self.patient))
+            print(json.dumps(self.patient))
+            self.current_patients += 1
+        connection.close()
