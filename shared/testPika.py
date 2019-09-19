6764a07e214958873ffb54d712e07c9e1b15261e
LiranNachman <lirannh@aidock.net>
2019-09-19 12:15:41 +0300

    first commit

---
+++ shared/testPika.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import pika
+import time, threading
+
+
+class Threaded_worker(threading.Thread):
+    def callback(self, ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        print("append to message array")
+        self.messages.append(body)
+        time.sleep(body.count(b'.'))
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def __init__(self, messages):
+        threading.Thread.__init__(self)
+        self.messages = messages
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue='task_queue', durable=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue='task_queue', on_message_callback=self.callback)
+
+    def run(self):
+        print('start consuming')
+        self.channel.start_consuming()
+
+
+if __name__ == "__main__":
+    messages = []
+    td = Threaded_worker(messages)
+    td.setDaemon(False)
+    td.start()
+    i = 0
+    while i < 100000:
+        print("here")
+        time.sleep(5)
+        print(messages)
+        i += 1
