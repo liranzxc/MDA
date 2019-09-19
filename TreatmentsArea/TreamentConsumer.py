2edff87e32d02739e35178b60dc0bcde7c833074
LiranNachman <lirannh@aidock.net>
2019-09-19 15:59:48 +0300

    THREADMENTS

---
+++ TreatmentsArea/TreamentConsumer.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import pika
+import time, threading
+import json
+
+class ThreamentWorker(threading.Thread):
+    def callback(self, ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        print("append to message array")
+        body = json.loads(body.decode('utf-8')) # decode json
+        print(body)
+        if(body["type"] == 'u'):
+            self.u_p.append(body)
+        if (body["type"] == 'n'):
+            self.non_u_p.append(body)
+        if (body["type"] == 'd'):
+            self.dead_p.append(body)
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def __init__(self, u_p,non_u_p,dead_p):
+        threading.Thread.__init__(self)
+        self.u_p = u_p
+        self.non_u_p = non_u_p
+        self.dead_p = dead_p
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue='Patients_queue', durable=True)
+        print(' [*] Waiting for patients. To exit press CTRL+C')
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue='Patients_queue', on_message_callback=self.callback)
+
+    def run(self):
+        print('start consuming')
+        self.channel.start_consuming()
+
+
+if __name__ == "__main__":
+    U_patients = []
+    Non_U_patients = []
+    DEAD_U_patients = []
+
+    td = ThreamentWorker(U_patients,Non_U_patients,DEAD_U_patients)
+    td.setDaemon(False)
+    td.start()
+
+    TIME_de
+
+    i = 0
+    while i<1000000:
+        print(U_patients)
+        print(Non_U_patients)
+        print(DEAD_U_patients)
+        time.sleep(5)
+        i=+1
+
